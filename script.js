96cdef00ec84df8793e9e1edf84f24dabedf87b3
suman <dhakalsumn739@gmail.com>
2019-12-26 14:39:57 +0545

    radio btn functionality is added

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
-var plainText = document.getElementById('plainText');
 var cipherText = document.getElementById('cipherText'); 
+var plainText = document.getElementById('plainText');
 var encryptBtn = document.getElementById('encryptBtn');
 var decryptBtn = document.getElementById('decryptBtn');
 var resultShow =  document.getElementById('result'); 
@@ -9,6 +9,74 @@ var cipherText2 = document.getElementById('cipherText2');
 var analyseBtn = document.getElementById('analyseBtn');
 var processing = document.getElementById('process');
 
+
+
+
+
+function encryption(){
+	var encryptionType = document.querySelector('input[name="cipher"]:checked').value;
+	var keyValue = Number(document.getElementById('key').value);
+	var plainText = document.getElementById('plainText').value;
+	
+	switch(encryptionType){
+	
+			case "crips":
+							cripsEncryption(plainText,keyValue);
+							break;
+			case "next":
+							alert("error");
+							break;
+				
+			default :
+						alert("error");
+
+		}
+
+}
+
+
+
+
+
+function decryption(){
+	var encryptionType = document.querySelector('input[name="cipher"]:checked').value;
+	var keyValue = Number(document.getElementById('key').value);
+	var cipherText = document.getElementById('cipherText').value; 
+
+	switch(encryptionType){
+		case "crips":
+				cripsDecryption(cipherText,keyValue);
+				break;
+		case "next":
+				alert("error");
+				break;
+		default :
+				alert("error");
+	}
+
+}
+
+
+
+
+
+function startCryptAnalysis(){
+	var encryptionType = document.querySelector('input[name="cipher"]:checked').value;
+	var ciphertxt = cipherText2.value;
+	switch(encryptionType){
+
+		case "crips": cripsCryptAnalysis(ciphertxt);
+							break;
+		case "next" : 
+						alert("ok !1");
+						break;
+
+		default :
+				alert("error");
+	}
+
+}
+//-------------- button enable disable functions -----------------
 
 function disableDecrypt(){
 	
@@ -33,44 +101,6 @@ function disableEncrypt(){
 
 }
 
-function encryption(){
-	var keyValue = Number(document.getElementById('key').value);
-	var ciphertxt ='' ;
-	
-	
-	for( i = 0; i < plainText.value.length ; i++){
-
-		ciphertxt += String.fromCharCode(((Number((plainText.value[i]).toUpperCase().charCodeAt(0))-65)+keyValue)%26 + 65);
-		
-		
-	}
-
-	resultShow.innerText = ciphertxt;
-
-
-
-}
-
-
-
-function decryption(){
-
-	var keyValue = Number(document.getElementById('key').value);
-	var plaintxt ='' ;
-	
-	
-	for( i = 0; i < cipherText.value.length ; i++){
-	
-		plaintxt += String.fromCharCode(((Number((cipherText.value[i]).toUpperCase().charCodeAt(0))-65)-keyValue+26)%26 + 65);	
-
-	}
-
-	resultShow.innerText = plaintxt;
-
-
-}
-
-
 function enableBtn(){
 	if (cipherText2.value.length > 0){
 		analyseBtn.disabled = false;
@@ -79,17 +109,47 @@ function enableBtn(){
 		analyseBtn.disabled = true;
 }
 
-function startCryptAnalysis(){
+
+
+
+
+//-------------------------------------------------------
+//-----------------algorithms ------------------------
+//-----------------------------------------------------
+function cripsEncryption(plainText,keyValue){
+	var ciphertxt ='' ;
+	for( i = 0; i < plainText.length ; i++){
+			ciphertxt += String.fromCharCode(((Number((plainText[i]).toUpperCase().charCodeAt(0))-65)+keyValue)%26 + 65);
+			}
+
+		resultShow.innerText = ciphertxt;
+
+		}
+
+
+function cripsDecryption(cipherText,keyValue){
+
+	var plaintxt ='' ;
 	
+	for( i = 0; i < cipherText.length ; i++){
+	
+		plaintxt += String.fromCharCode(((Number((cipherText[i]).toUpperCase().charCodeAt(0))-65)-keyValue+26)%26 + 65);	
+
+	}
+
+	resultShow.innerText = plaintxt;
+}
+	
+
+
+function cripsCryptAnalysis(cipherText){
 	var plaintxt;
-	processing.value = '';
+	processing.value = ''; //processing display box 
 	for (keyValue = 0 ; keyValue < 26 ; keyValue++){
 		plaintxt = ''; 
+		for (i = 0 ; i < cipherText.length ; i++)
+			plaintxt += String.fromCharCode(((Number((cipherText[i]).toUpperCase().charCodeAt(0))-65)-keyValue+26)%26 + 65);	
 
-		for (i = 0 ; i < cipherText2.value.length ; i++)
-			plaintxt += String.fromCharCode(((Number((cipherText2.value[i]).toUpperCase().charCodeAt(0))-65)-keyValue+26)%26 + 65);	
- 	 
-	console.log(keyValue);
 	processing.value += plaintxt + ", ";
 
 	}
